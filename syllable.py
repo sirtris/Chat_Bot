1f9e18ac1a7a79ac6b41c2c39bec4ca671933a58
Mathis Sackers <m.sackers@student.ru.nl>
2018-03-27 14:54:13 +0200

    handle when both beginning and end don't fit; Handle when there are no fitting synonyms at all

--- syllable.py
+++ syllable.py
@@ -9,18 +9,24 @@ To finish what you
 @author: tpaye
 """
 
+# import nltk
+# nltk.download('cmudict')
+
 from nltk.corpus import cmudict
 import pyphen
 import re, string
 from gensim.models import KeyedVectors
+
 #%%
 # load the Stanford GloVe model
 filename = 'glove.6B.50d.txt.word2vec'
 model = KeyedVectors.load_word2vec_format(filename, binary=False)
+
 print('Done loading model')
 #%%
 cmu_dict = cmudict.dict()
 pyphen_dict = pyphen.Pyphen(lang='en')
+
 
 def remove_punctuation(s):
     regex = re.compile('[%s]' % re.escape(string.punctuation))
@@ -48,6 +54,7 @@ def syl_in_words(words):
         counter += nsyl(word)
     return counter
 
+
 # counts number of syllables in sentence
 def syl_in_sentence(sentence):
     sentence = remove_punctuation(sentence)
@@ -64,11 +71,13 @@ def beginning_is_n_syllables(words, n):
         elif sub_counter > n:
             return False
 
+
 def words_to_sentence(ws):
     s = ""
     for w in ws:
         s += w + " "
     return s[:-1]
+
 
 # A Haiku is a short poem with 17 syllables aranged in a 5-7-5 pattern
 def is_haiku(sentence):
@@ -84,6 +93,7 @@ def is_haiku(sentence):
             sub_counter = 0
             line_counter += 1
     return line_counter == 3
+
 
 # when you give a sentece(haiku) the function formats it
 def format_haiku(sentence):
@@ -104,6 +114,7 @@ def format_haiku(sentence):
     else:
         return "Not a Haiku: " + sentence
 
+
 def cut_off(words, n):
     sub_counter = 0
     short_words = []
@@ -112,7 +123,8 @@ def cut_off(words, n):
         if sub_counter > n:
             short_words.append(word)
     return short_words
-            
+
+
 def cut_out(words, n):
     sub_counter = 0
     short_words = []
@@ -120,7 +132,8 @@ def cut_out(words, n):
         sub_counter += nsyl(word)
         short_words.append(word)
         if sub_counter == n:
-            return short_words            
+            return short_words
+
 
 def make_length_n(words, n):
     m = syl_in_words(words)
@@ -141,6 +154,8 @@ def make_length_n(words, n):
             new_words[i] = new_word
             if new_word != word:
                 m += nsyl(new_word) - nsyl(word)
+        if words == new_words:
+            return False
         words = new_words[:]
     return new_words
 
@@ -155,29 +170,61 @@ def cut_half(ws):
         if m > n/2:
             second_half.append(w)
         else:
-            first_half(w)
+            first_half.append(w)
     return first_half, second_half
 
 
+def cut_thirds(ws):
+    first_third = []
+    second_third = []
+    third_third = []
+    n = syl_in_words(ws)
+    m = 0
+    for w in ws:
+        m += nsyl(w)
+        if m < n/3:
+            first_third.append(w)
+        elif m > 2*(n/3):
+            second_third.append(w)
+        else:
+            third_third.append(w)
+    return first_third, second_third, third_third
+
+
 def modify_words(words, b, m, e):
+    bws = []
+    mws = []
+    ews = []
     if b and m:
-        return cut_out(words, 12) + make_length_n(cut_off(words, 12), 5)
+        ews = make_length_n(cut_off(words, 12), 5)
+        bws = cut_out(words, 12)
     elif e and m:
-        return make_length_n(reversed(cut_off(reversed(words), 12), 5)) + (cut_out(reversed(words), 12))[::-1]
+        bws = make_length_n(reversed(cut_off(reversed(words), 12), 5))
+        ews = (cut_out(reversed(words), 12))[::-1]
     elif b and e:
-        return cut_out(words, 5) + make_length_n(cut_off(reversed(cut_off(reversed(words), 5)), 5), 7) + (cut_out(reversed(words), 5))[::-1]
+        mws = make_length_n(cut_off(reversed(cut_off(reversed(words), 5)), 5), 7)
+        bws = cut_out(words, 5)
+        ews = (cut_out(reversed(words), 5))[::-1]
     elif b:
         mws, ews = cut_half(cut_off(words, 5))
-        return cut_out(words, 5) + make_length_n(mws, 7) + make_length_n(ews, 5)
+        mws = make_length_n(mws, 7)
+        ews = make_length_n(ews, 5)
+        bws = cut_out(words, 5)
     elif e:
         mws, bws = cut_half(cut_off(reversed(words), 5))
         bws = bws[::-1]
         mws = mws[::-1]
-        return make_length_n(bws, 5) + make_length_n(mws, 7) + (cut_out(reversed(words), 12))[::-1]
-    elif m:
-        return ["middle", "fits"]
+        bws = make_length_n(bws, 5)
+        mws = make_length_n(mws, 7)
+        ews = (cut_out(reversed(words), 12))[::-1]
     else:
-        return ["nothing", "fits"]
+        bws, mws, ews = cut_thirds(words)
+        bws = make_length_n(bws, 5)
+        mws = make_length_n(mws, 7)
+        ews = make_length_n(ews, 5)
+    if bws is False or mws is False or ews is False:
+        return ["can't", "find", "synonyms"]
+    return bws + mws + ews
 
 
 def generate_haiku(sentence):
@@ -203,10 +250,10 @@ def generate_haiku(sentence):
         return format_haiku(words_to_sentence(words))
     else:
         return "This is too long or too short to be a Haiku"
-    
-    
+
+
 def main():
     pass
-    
+
 if __name__ == '__main__':
     main()
